70a651a1c24fa802c9e4136082d81c4755fdf21c
Bryan Ortiz <bryan.ortiz@holinsys.pe>
2021-12-02 22:12:46 -0500

    docker

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BACKEND_URL: "http://localhost:3000",
+  BACKEND_URL: "http://157.245.136.250:3000",
   GESTION_URL: "http://localhost:4200",
 };
 
